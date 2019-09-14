53c9751c35432178012e03a5b8b214885c7015e3
Alex U <36997715+oogl@users.noreply.github.com>
2019-09-14 19:56:54 +0300

    Update FriendController.java
    Add GET params handler

--- friendlist/friendlist/src/main/java/com/example/friendlist/web/FriendController.java
+++ friendlist/friendlist/src/main/java/com/example/friendlist/web/FriendController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.friendlist.domain.Friend;
 
@@ -20,18 +21,19 @@ public class FriendController {
 	}
 	
 	@RequestMapping(value = "/index")
-	public String friendList(Model model) {
-		model.addAttribute("friend", new Friend());
+	public String friendList(@RequestParam(value="friend") String fname,
+							 Model model) {
+		model.addAttribute("friend", new Friend(fname));
 		model.addAttribute("friends", friends);
 		return "index";
 	}
 	
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String save(Friend friend,
-    				   String name,
-    				   Model model) {
+	public String save(Friend friend,
+					   String name,
+					   Model model) {
 		friend.setName(name);
 		friends.add(friend);
-        return "redirect:index";
+		return "redirect:index?friend=" + name.toString();
     }    
 }
